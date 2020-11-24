80838d5b630cf4972172b9a31287143cb7a66c52
KingstonMendoza <74968789+KingstonMendoza@users.noreply.github.com>
2020-11-24 22:54:52 +0800

    Add files via upload

---
+++ Photo album/High School.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <title>Londres- Photo Album: High School</title>
+	  <meta charset="UTF-8">
+		<link rel="stylesheet" type="text/css" href="Style3.css" />
+		<link href="https://fonts.googleapis.com/css2?family=Righteous&display=swap" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css2?family=Staatliches&display=swap" rel="stylesheet">
+   </head>
+   
+   <body>
+    
+	<div class="header">
+	 <h1>High School</h1>
+	 <hr style='width:50%;' />
+	 <h2 style='color: #000000;'>Londres, John Hendrics</h2>
+	</div>
+	  
+
+<div class="responsive">
+  <div class="gallery">
+    <a href="img_5terre.jpg">
+      <img src="Picture/w1.jpg" alt="Cinque Terre" width="600" height="400">
+    </a>
+    <div class="desc">Add a description of the image here</div>
+  </div>
+</div>
+
+
+<div class="responsive">
+  <div class="gallery">
+    <a href="Picture/Koala.jpg">
+      <img src="Picture/w2.jpg" alt="Forest" width="600" height="400">
+    </a>
+    <div class="desc">Add a description of the image here</div>
+  </div>
+</div>
+
+<div class="responsive">
+  <div class="gallery">
+    <a href="img_lights.jpg">
+      <img src="Picture/w3.jpg" alt="Northern Lights" width="600" height="400">
+    </a>
+    <div class="desc">Add a description of the image here</div>
+  </div>
+</div>
+
+<div class="responsive">
+  <div class="gallery">
+    <a href="img_mountains.jpg">
+      <img src="Picture/w4.jpg" alt="Mountains" width="600" height="400">
+    </a>
+    <div class="desc">Add a description of the image here</div>
+  </div>
+</div>
+
+<div class="clearfix"></div>
+
+<div style="padding:6px;">
+  <p>Sorry Sir idon have picture in Elementary. thank you!</p>
+  <p>You will learn more about media queries and responsive web design later in our CSS Tutorial.</p>
+</div>
+		
+   </body>
+</html>
---
+++ Photo album/Index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <title>Londres- Photo Album</title>
+	  <meta charset="UTF-8">
+		<link rel="stylesheet" type="text/css" href="Style3.css" />
+		<link href="https://fonts.googleapis.com/css2?family=Righteous&display=swap" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css2?family=Staatliches&display=swap" rel="stylesheet">
+   </head>
+   
+   <body>
+    
+	<div class="header">
+	 <h1>My Pictures </h1>
+	 <hr style='width:50%;' />
+	 <h2 style='color: #000000;'>Londres, John Hendrics</h2>
+	</div>
+	  
+	  <div class="responsive">
+  <div class="gallery">
+    <a href="Elementary.php">
+      <img src="Picture/nani.jpg" alt="nani?" width="600" height="400">
+    </a>
+    <div class="desc">Elementary</div>
+  </div>
+</div>
+
+<div class="responsive">
+  <div class="gallery">
+    <a href="High School.php">
+      <img src="Picture/meh.jpg" alt="Northern Lights" width="600" height="400">
+    </a>
+    <div class="desc">High School</div>
+  </div>
+</div>
+
+<div class="responsive">
+  <div class="gallery">
+    <a  href="Senior High.php">
+      <img src="Picture/dim.jpg" alt="Mountains" width="600" height="400">
+    </a>
+    <div class="desc">Senior High</div>
+  </div>
+</div>
+
+<div class="responsive">
+  <div class="gallery">
+    <a  href="College.php">
+      <img src="Picture/dim.jpg" alt="Mountains" width="600" height="400">
+    </a>
+    <div class="desc">College</div>
+  </div>
+</div>
+
+<div class="clearfix"></div>
+
+<div style="padding:6px;">
+  <p>I dont have picture in Elementary so enjoy!!!!</p>
+</div>
+
+  <!-- Site footer -->
+    <footer class="footer">
+	<h5>Thanks For Visiting</h5>
+	<hr/>
+	</footer>
+		
+   </body>
+</html>
